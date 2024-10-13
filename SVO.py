4125a841c2191a43de26dcb0ae54d24e9c473e50
AlexandrFedorow <alexandr.fedoroff@icloud.com>
2024-10-14 00:14:19 +0300

    voen

--- SVO.py
+++ SVO.py
@@ -19,24 +19,82 @@ def pvo(c0, cd, hd, hi, o):
     g = (cd - c0) / hd
     ci = c0 + g * hi
     cpvo = ci/math.cos(math.radians(o/2))
+    hpvo = 0
     if cpvo > c0:
         print('ПВО нет')
-    return cpvo
+        hpvo = 0
+    return ci, hpvo
 
 
-def kvl(pd):
-    pass
+def kvl(c, hi, o, hp, hd, hpvo, n):
+    h = []
+    r = []
+
+    for i in range(n-1):
+        o.append(math.degrees(math.acos(math.cos(math.radians(o[len(o)-1])) * c[i+1] / c[i])))
+
+    if hpvo == 0:
+        h = [hi, hd]
+    else:
+        h = [abs(hd - hpvo - hi), hd]
+
+    for i in range(n-1):
+        r.append(h[i]/math.tan(math.radians((o[i]+o[i+1])/2)))
+
+    return o, r, h
 
 
-def knl():
-    pass
+def knl(c, hi, o, hp, hd, hpvo, n):
+    h = []
+    r = []
+
+    for i in range(n - 1):
+        o.append(math.degrees(math.acos(math.cos(math.radians(o[len(o) - 1])) * c[i + 1] / c[i])))
+
+    if hpvo == 0:
+        h = [hd-hi, hd]
+    else:
+        h = [hd-hi, hpvo]
+
+    for i in range(n - 1):
+        r.append(h[i] / math.tan(math.radians((o[i] + o[i + 1]) / 2)))
+
+    return o, r, h
 
 
-def plot_points(points, title, subplot):
-    x_vals = [point for point in points[0]]
-    y_vals = [point for point in points[1]]
+def make_coords(r, h, hi, hd, hpvo, ch):
+    coords = [[0],[hi]]
+    if hpvo == 0:
+        for i in range(len(r)):
+            coords[0].append(coords[0][i]+r[i])
+            if ch:
+                coords[1].append(hd)
+                ch = False
+            else:
+                coords[1].append(0)
+                ch = True
+    else:
+        for i in range(len(r)):
+            coords[0].append(coords[0][i]+r[i])
+            if ch:
+                coords[1].append(hpvo)
+                ch = False
+            else:
+                coords[1].append(hd)
+                ch = True
+
+    return coords
+
+
+def plot_points(points1, points2, title, subplot, ch):
+    x_vals = [point for point in points1[0]]
+    y_vals = [point for point in points1[1]]
 
     subplot.plot(x_vals, y_vals, color='blue')
+    if ch:
+        x_vals = [point for point in points2[0]]
+        y_vals = [point for point in points2[1]]
+        subplot.plot(x_vals, y_vals, color='red')
 
     subplot.set_xlim(min(x_vals), max(x_vals))
     subplot.set_ylim(min(y_vals), max(y_vals))
@@ -51,15 +109,23 @@ def plot_points(points, title, subplot):
     subplot.grid(True)
 
 
-# Параметры
-print(pvo(c0, cd, hd, hi, o))
 
-# Визуализация
+ci, hpvo = pvo(c0, cd, hd, hi, o)
+print(ci)
+o1, r, h = kvl([ci, c0, cd], hi, [o/2], hp, hd, hpvo, 3)
+coords1 = make_coords(r, h, hi, hd, hpvo, False)
+
+o1, r, h = knl([ci, c0, cd], hi, [o/2], hp, hd, hpvo, 3)
+coords2 = make_coords(r, h, hi, hd, hpvo, True)
+
+
+
 fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 
 
-plot_points([[c0, cd], [0, 1000]], 'Вертикальное распределение скорости звука', axs[0], )
-plot_points(cord1, 'Траектория лучей в подводном канале', axs[1])
+plot_points([[c0, cd], [0, 1000]], [], 'Вертикальное распределение скорости звука', axs[0], False)
+plot_points(coords1, coords2, 'Траектория лучей в подводном канале', axs[1], True)
+
 
 plt.tight_layout()
 plt.show()
